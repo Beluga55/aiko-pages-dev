658fe08774571836d21d38f07b26bcd46ba07630
beluga55 <beluga.github@gmail.com>
2022-06-26 11:37:44 +0800

    Added Icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
   origin: "top",
-  distance: "60px",
+  distance: "55px",
   duration: 2500,
   delay: 400,
 });
@@ -11,5 +11,6 @@ sr.reveal( `.card__content-1`, {delay: 500})
 sr.reveal( `.card__content-2`, {delay: 600})
 sr.reveal( `.about__container`, {delay: 700})
 sr.reveal( `.social__container`, {delay: 900})
+sr.reveal(`.footer__wrapper`, {origin: "left"}, {delay: 1000})
 sr.reveal( `.games__movies-first`, {origin: 'left'})
 sr.reveal( `.games__movies-second`, {origin: 'right'})
